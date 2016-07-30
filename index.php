2c8c813ae5f8611d4fcd524d7147d8f18c6fff4f
Polina <polina200789@bk.ru>
2016-07-30 11:25:13 +0300

    Базовый проект

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Сайт</title>
+
+		<link rel="stylesheet" href="theme/js/fancybox/jquery.fancybox.css">
+
+		<link rel="stylesheet" href="theme/css/styles.css">
+
+		<script src="theme/js/libs.js"></script>
+		<script src="theme/js/main.js"></script>
+	</head>
+	<body>
+		<header>
+			<nav>Основное меню навигации</nav>
+		</header>
+		<section>
+			<aside>
+				Дополнительное меню
+			</aside>
+			Основной контент
+		</section>
+		<footer>
+			Футер
+		</footer>
+	</body>
+</html>
